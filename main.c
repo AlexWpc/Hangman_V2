3c6d2b1f7524fb1a26a65cea81e855c51f46b0d9
AlexWpc <dok-chernikov@mail.ru>
2020-09-29 01:55:16 +0700

    Add menu function

--- main.c
+++ main.c
@@ -1,4 +1,24 @@
+#include <stdio.h>
+
+void menu (void);
+
 int main()
 {
-	return 0;
+menu();
+
+return 0;
 }
+void menu (void) {
+    printf(" _                                             \n");
+    printf("| |                                            \n");
+    printf("| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  \n");
+    printf("| '_ \\ / _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\ \n");
+    printf("| | | | (_| | | | | (_| | | | | | | (_| | | | | \n");
+    printf("|_| |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_| \n");
+    printf("                    __/ |                      \n");
+    printf("                   |___/  \n");
+    printf("\n");
+    printf("By Alexey Chernikov and Andrey Gondaruk (IS_942)\n");
+    printf("\n");
+}
+
