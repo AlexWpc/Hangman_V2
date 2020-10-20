18776cb0e36210e9815ff25a4fe543dcce0706d8
CentralOpera <666shampur@gmail.com>
2020-10-20 21:37:46 +0700

    Created head-file for main.c

---
+++ src/functions.h
@@ -0,0 +1,13 @@
+#ifndef functions_H
+#define functions_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void menu();
+
+int checkLowercase(char *inputString);
+
+void hangman(int attempt);
+
+#endif
