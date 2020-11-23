5c92a46bca848ae7866f599ecc8b014fe2efd575
CentralOpera <666shampur@gmail.com>
2020-11-24 00:49:44 +0700

    Added new functions to header file

--- src/functions.h
+++ src/functions.h
@@ -1,15 +1,26 @@
 #ifndef functions_H
 #define functions_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-void menu();
+#define FALSE 0
+#define TRUE 1
+#define MAX_LENGTH 101
 
 int checkLowercase(char* inputString);
 
 void hangman(int attempt);
 
 void replayGame(char decision[], int* decided, int* replay);
+
+int ScanNumOfWords(int numOfWords);
+
+int CreateArray(int i, char* wordList, int j);
+
+int InputLetters(char letter[], int length);
+
+int LetterCheck(int length, char word[], char letter[], int attempt, int checkArray[]);
+
+void ChecktoWork(int length, int checkArray[], int workArray[]);
+
+int SolvedLetterCheck(int length, int workStatus, int workArray[]);
 
 #endif
